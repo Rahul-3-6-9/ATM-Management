86e4db74861ecc8ce17ee5f250a3968428cc7cf6
Rahul-3-6-9 <159149712+Rahul-3-6-9@users.noreply.github.com>
2025-05-07 16:40:28 +0530

    Update Main Code.py

--- ATM Management with SQL/Main Code.py
+++ ATM Management with SQL/Main Code.py
@@ -1,8 +1,8 @@
 import getpass
 import string
 import os
-
 import db_operations
+
 
 print("========================================")
 print("........................................")
@@ -10,27 +10,31 @@ print("       WELCOME TO THE ATM SYSTEM        ")
 print("........................................")
 print("========================================")
 
-
+wrong_input = 0
 while True:
-    entry_Choice = int(input("If You Want To Register A User: Press 1 \nIf You Are Already Registered: Press 2 \n"))
-    if entry_Choice==1:
-        id = db_operations.last_user_id()[0] + 1
+    entry_Choice = input("If You Want To Register A User: Press 1 \nIf You Are Already Registered: Press 2 \nIf You Want to Delete an Account: Press 3 \n")
+    if entry_Choice == '1':
         print(":::::::::::::::::::::::::")
-        first_name = input("ENTER YOUR FIRST NAME : ")
-        last_name = input("ENTER YOUR LAST NAME : ")
+        id = int(input("ENTER YOUR ACCOUNT NUMBER: "))
+        while (id in db_operations.get_all_user_ids()):
+            print("!!!ENTER THE CORRECT ACCOUNT NUMBER!!!")
+            id = int(input("ENTER YOUR ACCOUNT NUMBER: "))
+
+        first_name = input("ENTER YOUR FIRST NAME : ").lower()
+        last_name = input("ENTER YOUR LAST NAME : ").lower()
 
         print("ENTER YOUR PIN (4 DIGITS) : ", end="")
         while True:
             pin = int(input())
-            if (pin>=1000 and pin<=9999):
+            if pin >= 1000 and pin <= 9999:
                 break
             else:
                 print("Wrong Format ... Please Enter Again", end="")
 
         print("ENTER YOUR GENDER(M - MALE /F - FEMALE /O - OTHERS /N - NOT WILLING TO MENTION) : ", end="")
         while True:
-            gender = input()
-            if (gender.upper() in ['M', 'F', 'O', 'N']):
+            gender = input().upper()
+            if gender in ['M', 'F', 'O', 'N']:
                 break
             else:
                 print("Invalid Choice ... Please Enter Again", end="")
@@ -38,44 +42,41 @@ while True:
         print("ENTER YOUR DEPOSIT(LIMIT = 0) : ", end="")
         while True:
             deposit = int(input())
-            if (deposit>0):
+            if deposit > 0:
                 break
             else:
                 print("Below Limit ... Please Enter Again", end="")
 
         print("User Added Successfully")
-        print(":::::::::::::::::::::::::\n\n")
+        print(":::::::::::::::::::::::::\n\n2")
         db_operations.add_entry_to_users(id, first_name, last_name, pin, gender, deposit)
 
+    elif entry_Choice == '2':
+        user_id = input("Enter your Account Number: ")
+        user_id = int(user_id)
 
-
-    elif entry_Choice == 2:
-        firstname = input("Enter your first name : ")
-        firstname = firstname.lower()
-        lastname = input("Enter your last name : ")
-        lastname = lastname.lower()
-        matching_list = db_operations.get_record(firstname, lastname)
+        matching_list = db_operations.get_user_by_id(user_id)
 
         if len(matching_list) == 0:
             print("---------------------")
-            print("   INVALID USERNAME  ")
+            print("   INVALID ACCOUNT   ")
             print("---------------------")
-            break;
+            break
 
         print(":::::::::::::::::::::::::")
         pin = input("ENTER YOUR PIN : ")
         print(":::::::::::::::::::::::::")
 
-        if db_operations.check_pin(firstname, lastname, pin) == False:
+        if db_operations.check_pin_by_id(user_id, pin) == False:
             print(":::::::::::::::::::::::::")
             print("PIN ENTERED IS INVALID")
             print(":::::::::::::::::::::::::")
-            break;
+            break
 
         print("*************************")
         print("     LOGIN SUCCESSFUL    ")
         print("*************************")
-        print(str.capitalize(firstname), " Welcome to MyBank ATM")
+        print("Welcome to MyBank ATM")
 
         while True:
             print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
@@ -83,11 +84,10 @@ while True:
                 "Select an option from the following that you would like to proceed with : Check Balance(c), Deposit(D), Withdraw(w), Change pin(p), Quit(q) : ")
             print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
-            # valid_responses = ['c', 'w', 'p', 'q', 'd']
             response = response.lower()
 
             if response == 'c':
-                balance_user = db_operations.check_balance(firstname, lastname)
+                balance_user = db_operations.check_balance_by_id(user_id)
                 print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
                 print("You have ", balance_user, " rupees in your account ")
                 print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
@@ -96,13 +96,14 @@ while True:
                 print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
                 withdraw = int(input("Enter the amount you would like to withdraw : "))
                 print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-                balance_user = db_operations.check_balance(firstname, lastname)
-
+                balance_user = db_operations.check_balance_by_id(user_id)
+                if response.lower() == 'return':
+                    continue
                 if withdraw > balance_user:
                     print("Insufficient balance : ")
                 else:
                     balance_user = balance_user - withdraw
-                    db_operations.update_balance(firstname, lastname, balance_user)
+                    db_operations.update_balance_by_id(user_id, balance_user)
                     print("The amount you have withdrawn is : ", withdraw)
                     print("The amount left in your account is : ", balance_user)
 
@@ -110,19 +111,23 @@ while True:
                 print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
                 deposit = int(input("Enter the amount you would like to deposit : "))
                 print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+                if response.lower() == 'return':
+                    continue
                 if deposit > 50000:
                     print("Deposit Limit Exceeded ...")
                 else:
-                    balance_user = db_operations.check_balance(firstname, lastname)
+                    balance_user = db_operations.check_balance_by_id(user_id)
                     balance_user = balance_user + deposit
-                    db_operations.update_balance(firstname, lastname, balance_user)
-                    print("The amount you have deposit is : ", deposit)
+                    db_operations.update_balance_by_id(user_id, balance_user)
+                    print("The amount you have deposited is : ", deposit)
                     print("The amount left in your account is : ", balance_user)
 
             elif response == 'p':
                 print("________________________________________________")
                 old_pin = input("Please enter your old pin : ")
                 print("________________________________________________")
+                if response.lower() == 'return':
+                    continue
 
                 if old_pin == pin:
                     print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
@@ -136,7 +141,7 @@ while True:
                         if pin_confirm != new_pin:
                             print("PIN MISMATCH ! ")
                         else:
-                            db_operations.change_pin(firstname, lastname, new_pin)
+                            db_operations.change_pin_by_id(user_id, new_pin)
                             print("New pin saved")
                     else:
                         print("Your pin must contain 4 digits and must be different from your original pin")
@@ -152,7 +157,34 @@ while True:
                 print("!!!!!!!!!!!!!!!!!!!!")
                 print("Response is not valid")
 
+    elif entry_Choice == '3':
+        print(":::::::::::::::::::::::::")
+        id = int(input("ENTER YOUR ACCOUNT NUMBER: "))
+        while (id not in db_operations.get_all_user_ids()):
+            print("!!!ENTER THE CORRECT ACCOUNT NUMBER!!!")
+            id = int(input("ENTER YOUR ACCOUNT NUMBER: "))
+
+        print(":::::::::::::::::::::::::")
+        pin = input("ENTER YOUR PIN : ")
+        print(":::::::::::::::::::::::::")
+
+        if db_operations.check_pin_by_id(id, pin) == False:
+            print(":::::::::::::::::::::::::")
+            print("PIN ENTERED IS INVALID")
+            print(":::::::::::::::::::::::::")
+            break
+
+        print("PLEASE CONFIRM THE DELETION OF THE ACCOUNT BY TYPING \"CONFIRM\"")
+        confirmation=input().lower()
+        if (confirmation=="confirm"):
+            db_operations.delete_user_by_id(id)
 
     else:
+        if wrong_input == 2:
+            print("!!!!!!!!!!!!!!!!!!!!")
+            print("You have failed 3 times .... Closing Session")
+            break
+
+        wrong_input += 1
         print("!!!!!!!!!!!!!!!!!!!!")
-        print("Response is not valid")
+        print("Response is not valid ... Tries left: ", 3 - wrong_input)
